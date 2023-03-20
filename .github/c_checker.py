ac724b2de2ba009e0ae0842d5e0aee2c3d3cb3dd
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-03-20 10:28:14 +0100

    fix: Stopped checking for cs erros in libs/myteams

--- .github/c_checker.py
+++ .github/c_checker.py
@@ -10,7 +10,7 @@ TOKENS_TXT = working_dir + "/.github/checker/tokens.txt"
 def getFiles():
     files = []
     # exclude files in bonus and tests folders
-    os.system("find . -name '*.c' -o -name '*.h' -not -path './bonus/*' -not -path './tests/*' > files.txt")
+    os.system("find . -name '*.c' -o -name '*.h' -not -path './bonus/*' -not -path './tests/*' -not -path './libs/myteams/*' > files.txt")
     with open("files.txt", "r") as f:
         files = f.read().split("\n")
         for (i, file) in enumerate(files):
